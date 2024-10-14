6e1506fdd7fc08836ce9b809d93adc7d0ccaebbb
Douglas Vanny <douglas.vanny@gmail.com>
2024-10-13 23:22:05 -0300

    Working

--- assets/character.gd
+++ assets/character.gd
@@ -4,10 +4,12 @@ extends Node3D
 @export var rotateSpeed:float;
 @export var attackDuration:float;
 
-@export var sprite:Sprite3D;
-
 @export var idleTexture:Texture2D;
 @export var attackTexture:Texture2D;
+
+@export_group("Node references")
+@export var sprite:Sprite3D;
+@export var noClipSprite:GeometryInstance3D;
 
 var navigationYRange:float = 0.5;
 
@@ -18,7 +20,7 @@ var attackDurationLeft:float;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
-	sprite.texture = idleTexture;
+	setTexture(idleTexture);
 
 var frameWait:int = 1;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,7 +32,7 @@ func _process(delta: float) -> void:
 	if attackDurationLeft > 0:
 		attackDurationLeft -= delta;
 		if attackDurationLeft <= 0:
-			sprite.texture = idleTexture;
+			setTexture(idleTexture);
 	
 	var newPosition = position;
 	
@@ -54,8 +56,27 @@ func _input(event: InputEvent) -> void:
 			movement.x -= 1.0 * pressed;
 		elif event.keycode == KEY_D:
 			movement.x += 1.0 * pressed;
-		elif event.keycode == KEY_SPACE:
-			attackDurationLeft = attackDuration;
-			sprite.texture = attackTexture;
+		elif event.pressed:
+			if event.keycode == KEY_SPACE and attackDuration > 0.0:
+				attackDurationLeft = attackDuration;
+				setTexture(attackTexture);
+			elif event.keycode == KEY_TAB:
+				noClipSprite.visible = !noClipSprite.visible;
+				sprite.visible = !noClipSprite.visible;
+			elif event.keycode == KEY_1:
+				sprite.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+				noClipSprite.material_override.set_shader_parameter("billboardMode", 0);
+			elif event.keycode == KEY_2:
+				sprite.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
+				noClipSprite.material_override.set_shader_parameter("billboardMode", 1);
+			elif event.keycode == KEY_3:
+				sprite.visible = false
+				noClipSprite.visible = true
+				sprite.billboard = BaseMaterial3D.BILLBOARD_DISABLED
+				noClipSprite.material_override.set_shader_parameter("billboardMode", 2);
 	elif event is InputEventMouseMotion:
 		cameraRotation -= event.relative.x;
+
+func setTexture(newTexture:Texture2D) -> void:
+	sprite.texture = newTexture;
+	noClipSprite.material_override.set_shader_parameter("sprite", newTexture);
